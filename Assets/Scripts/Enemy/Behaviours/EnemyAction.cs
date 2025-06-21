2ec3a4f869b50d83e2b825e41115c38ceea5e113
WMsans <ggajy123@gmail.com>
2025-06-21 10:34:06 -0400

    Behaviour tree import

---
+++ Assets/Scripts/Enemy/Behaviours/EnemyAction.cs
@@ -0,0 +1,15 @@
+using Opsive.BehaviorDesigner.Runtime.Tasks.Actions;
+using UnityEngine;
+
+public abstract class EnemyAction : Action
+{
+    protected Rigidbody2D rb;
+    protected Animator animator;
+    protected bool FacingRight => Mathf.Abs(Mathf.DeltaAngle(transform.rotation.y, 0f)) < 1f;
+
+    public override void OnAwake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponentInChildren<Animator>();
+    }
+}
---
+++ Assets/Scripts/Enemy/Behaviours/EnemyConditional.cs
@@ -0,0 +1,17 @@
+using Opsive.BehaviorDesigner.Runtime.Tasks.Conditionals;
+using UnityEngine;
+
+public abstract class EnemyConditional : Conditional
+{
+    protected Rigidbody2D rb;
+    protected Animator animator;
+    protected Harmable enemyHarmable;
+    protected bool FacingRight => Mathf.Abs(Mathf.DeltaAngle(transform.rotation.y, 0f)) < 1f;
+
+    public override void OnAwake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponentInChildren<Animator>();
+        enemyHarmable = gameObject.GetComponentInChildren<Harmable>();
+    }
+}
